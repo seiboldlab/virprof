434ce28fd0a355c0e1d420f4f24fd3af8291ce28
Elmar Pruesse <pruessee@njhealth.org>
2020-06-07 16:36:54 -0600

    Rewrote accidentally deleted parts of WordScorer

--- rules/classify.py
+++ rules/classify.py
@@ -850,6 +850,92 @@ class WordScorer:
         self.maxwordlen = maxwordlen
         self.orderweight = orderweight
         self.keepwords = keepwords(words)
+
+    @staticmethod
+    def split_words(chain: HitChain) -> Iterable[str]:
+        """Split title of ``chain`` into words"""
+        return chain.stitle.split()
+
+    @staticmethod
+    def strip_punctuation(words: Iterable[str]) -> Iterable[str]:
+        """Remove punctuation from the outside of each word
+
+        Args:
+          words: sequence of words
+        Returns:
+          Generator over words
+        """
+        for word in words:
+            yield word.strip(string.punctuation)
+
+    @staticmethod
+    def uncapitalize(words: Iterable[str]) -> Iterable[str]:
+        """Remove capitalization for words without internal caps
+
+        Args:
+          words: sequence of words
+        Returns:
+          Generator over words
+        """
+        for word in words:
+            if word[1:].isLower():
+                yield word.lower()
+            else:
+                yield word
+
+    @staticmethod
+    def combine_shortwords(words: Iterable[str]) -> Iterable[str]:
+        """Combine short words with their predecessor
+
+        Args:
+          words: sequence of words
+        Returns:
+          Generator over words
+        """
+        stack = []
+        for word in words:
+            if len(word) <= 2:
+                stack += [' '.join((stack[-1], word))]
+            else:
+                yield from reversed(stack)
+                stack  = [word]
+        yield from reversed(stack)
+
+    @staticmethod
+    def filter_stopwords(words: Iterable[str]) -> Iterable[str]:
+        """Filter out words occurring in our stopword list
+
+        Args:
+          words: sequence of words
+        Returns:
+          Generator over words
+        """
+        for word in words:
+            if word.lower() not in self.stopwords:
+                yield word
+
+    def score(self, chains: List[HitChain]) -> str:
+        """Generate summary using word scoring
+
+        Breaks the title of each passed in HitChain into words,
+        removes outside punctuation, removes captitalization, merges
+        short words with their preceding word(s) and removes
+        (singleton) stopwords.
+
+        Each word occurring first in any input title receives a score
+        equal to the sum of the log-evalues (exponent) of the
+        respective HitChain. The words and scores are added with each
+        subsequent word, dividing the log-evalue by ``orderweight`` as
+        we move through the title.
+
+        The resulting words are sorted by score and emitted as
+        result. Up to ``keepwords`` words may comprise the
+        result. Words composed from multiple words are split for this
+        accounting, and no (sub)word may occur twice.
+        """
+        for chain in chains:
+            words = self.split_words(words)
+            words = self.strip_punctuation(words)
             words = self.uncapitalize(words)
             words = self.combine_shortwords(words)
             words = self.filter_stopwords(words)
