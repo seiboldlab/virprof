82ca05a3d245306b1962be6960c6fdad4a8ed0d3
Elmar Pruesse <pruessee@njhealth.org>
2020-09-24 11:34:26 -0600

    Guard virprof module against execution with py <3.6

--- virprof/__init__.py
+++ virprof/__init__.py
@@ -1,1 +1,7 @@
 """Contains the custom code for blast based binning"""
+
+import sys
+if sys.version_info.major < 3 or sys.version_info.minor < 6:
+    print("ERROR: Python version must be at least 3.6!")
+    print("Found ", sys.version)
+    sys.exit(1)
