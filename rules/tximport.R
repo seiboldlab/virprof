c9cfdb89943ea8bf204008ada30ebf3427fddced
Elmar Pruesse <pruessee@njhealth.org>
2022-11-03 14:39:20 -0600

    fix: revert having all idcolumns - only one row id present

--- rules/tximport.R
+++ rules/tximport.R
@@ -557,7 +557,7 @@ if (snakemake@params$input_type == "ExonSE") {
             by = idcolumns[[1]]
         ) %>%
         transmute(
-            across(all_of(idcolumns)),
+            across(all_of(idcolumns[[1]])),
             mapped_read_count = count_total,
             pct_mito = count_mito / count_total * 100,
             pct_ribo = count_ribo / count_total * 100,
