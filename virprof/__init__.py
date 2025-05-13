75475e64c5279a274fabc5f04ed15c957497765e
Elmar Pruesse <pruessee@njhealth.org>
2025-05-13 14:22:09 -0600

    chore: packaging fixes

--- virprof/__init__.py
+++ virprof/__init__.py
@@ -1,1 +1,14 @@
 """Virprof pipeline custom functions for pathogen recovery"""
+
+from pathlib import Path
+
+def get_resource(path):
+    roots = [Path(__file__).parent.parent, Path(__file__).parent / "pipeline"]
+    for root in roots:
+        loc = root / path
+        if loc.exists():
+            return loc
+    raise Exception(f"Unable to find {path}. Please check your installation")
+
+def get_ymp_yml():
+    return str(get_resource("virprof.yml"))
